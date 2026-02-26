d659ff6d312bdfed8157711d68e0c34f0bf7451e
lucasbj2000 <lucmabeji2000@gmail.com>
2026-02-26 20:05:20 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // En Vercel funciona perfecto sin tocar nada.
+  // Si en el futuro quieres GitHub Pages, se suele setear base: "/repo/"
+});
